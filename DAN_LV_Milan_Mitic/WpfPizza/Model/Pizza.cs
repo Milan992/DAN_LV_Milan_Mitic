5391c597334f2ea3988a65bc0f370143b72ff2ec
Milan992 <mitic.gsm@gmail.com>
2020-08-24 13:09:36 +0200

    commit before merge.

---
+++ DAN_LV_Milan_Mitic/WpfPizza/Model/Pizza.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfPizza.Model
+{
+    class Pizza
+    {
+        public string Size { get; set; }
+        public List<Ingreedient> Ingreedients { get; set; }        
+
+        public override string ToString()
+        {
+            string ingreedients = "";
+            foreach (var ingreedient in Ingreedients)
+            {
+                ingreedients += ingreedient.Name+", ";
+            }
+            return Size + ": " + ingreedients;
+        }
+
+        public string GetPrice(Pizza pizzaToOrder)
+        {
+            int amount = 0;
+            foreach (var ingreedient in pizzaToOrder.Ingreedients)
+            {
+                amount += ingreedient.Price;
+            }
+            if (pizzaToOrder.Size == "small")
+            {
+                amount += 300;
+            }
+            else if (pizzaToOrder.Size == "medium")
+            {
+                amount += 500;
+            }
+            else if (pizzaToOrder.Size == "big")
+            {
+                amount += 700;
+            }
+            return amount.ToString();
+        }
+    }
+}
