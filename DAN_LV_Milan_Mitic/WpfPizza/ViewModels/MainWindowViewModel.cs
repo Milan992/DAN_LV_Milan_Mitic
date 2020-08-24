08ac4e7dac2da3d4318c9ccedc7eed0aff3c57a0
Milan992 <mitic.gsm@gmail.com>
2020-08-24 09:23:57 +0200

    Created ViewModelBase class.

---
+++ DAN_LV_Milan_Mitic/WpfPizza/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WpfPizza.ViewModels
+{
+    class MainWindowViewModel
+    {
+        Service service = new Service();
+        MainWindow main;
+
+        #region Constructors
+
+        public MainWindowViewModel(MainWindow mainOpen)
+        {
+            main = mainOpen;
+        }
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Commands
+
+        private ICommand calculateAmount;
+
+        public ICommand CalculateAmount
+        {
+            get
+            {
+                if (calculateAmount == null)
+                {
+                    calculateAmount = new RelayCommand(param => CalculateAmountExecute(), param => CanCalculateAmountExecute());
+                }
+
+                return calculateAmount;
+            }
+        }
+
+        private void CalculateAmountExecute()
+        {
+            try
+            {
+               
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private bool CanCalculateAmountExecute()
+        {
+            return true;
+        }
+
+        #endregion
+    }
+}
